dc093b553b558cb486ef75869a69690c01d0a20f
Preston Cessna <Prestonc.ECTS@gmail.com>
2023-12-19 10:43:40 -0500

    works

--- Module1.vb
+++ Module1.vb
@@ -7,9 +7,11 @@ Module Module1
     Dim usermoves(4), computermoves(4) As Integer
     Dim outcomes(4) As String
     Dim num As Integer
+    Dim word As String
 
     Sub Main()
         Dim Input As Integer
+
         Console.WriteLine(".-. .-. .-. . .     .-. .-. .-. .-. .-.     .-. .-. .-. .-. .-. .-. .-. .-.
 |(  | | |   |<      |-' |-| |-' |-  |(      `-. |    |  `-. `-. | | |(  `-.
 ' ' `-' `-' ' ` ,   '   ` ' '   `-' ' ' ,   `-' `-' `-' `-' `-' `-' ' ' `-'
@@ -23,7 +25,7 @@ Module Module1
         For i As Integer = 0 To 4
             usermoves(i) = getvalidinput()
             computermoves(i) = COMPUTERMOVE()
-            Console.WriteLine($"You PLayed {numtoword(usermoves(i))} the computer played {numtoword(computermoves(i))}")
+            Console.WriteLine($"You Played {numtoword(usermoves(i))} the computer played {numtoword(computermoves(i))}")
             outcomes(i) = determineoutcome(usermoves(i), computermoves(i))
             Console.WriteLine($"You {outcomes(i)}")
         Next
@@ -57,7 +59,6 @@ Module Module1
         Return guess
     End Function
     Function numtoword(num As Integer) As String
-        Dim word As String = "UNDEFINED"
         If num = 1 Then
             word = "ROCKA"
         ElseIf num = 2 Then
@@ -68,6 +69,8 @@ Module Module1
         Return word
     End Function
     Function determineoutcome(userplay As Integer, compplay As Integer) As String
+
+
         Dim outcome As String = "Lost"
         If (userplay = 1 AndAlso compplay = 3) OrElse
        (userplay = 2 AndAlso compplay = 1) OrElse
@@ -80,11 +83,27 @@ Module Module1
         End If
         Return outcome
     End Function
+    ''' <summary>
+    ''' Prints Table With Reports
+    ''' </summary>
     Sub PRINTBOARD()
         Console.WriteLine()
-        Console.WriteLine("".PadRight(90, "#"))
+        Console.WriteLine("".PadRight(77, "#"))
         Console.WriteLine("{0}{1}{2}", "".PadLeft(23, "#"), " Rocka, Paper, Scissors Report ", "".PadRight(23, "#"))
-        Console.WriteLine("".PadRight(90, "#"))
+        Console.WriteLine("".PadRight(77, "#"))
+        Console.WriteLine("".PadRight(90, "_"))
+        Console.WriteLine("{0}{1}{2}{3}{4}{5}{6}{7}", "|".PadRight(5), "Round".PadRight(20), "|".PadRight(5), "User Played".PadRight(20), "|".PadRight(5), "Computer Played".PadRight(20), "|".PadRight(5), "Outcome".PadRight(20), "|")
+        Console.WriteLine("".PadRight(90, "_"))
+        Console.WriteLine("{0}{1}{2}{3}{4}{5}{6}{7}", "|".PadRight(5), "1".PadRight(20), "|".PadRight(5), numtoword(usermoves(0)).ToString.PadRight(20), "|".PadRight(5), numtoword(computermoves(0)).ToString.PadRight(20), "|".PadRight(5), outcomes(0).PadRight(20), "|")
+        Console.WriteLine("".PadRight(90, "_"))
+        Console.WriteLine("{0}{1}{2}{3}{4}{5}{6}{7}", "|".PadRight(5), "2".PadRight(20), "|".PadRight(5), numtoword(usermoves(0)).ToString.PadRight(20), "|".PadRight(5), numtoword(computermoves(1)).ToString.PadRight(20), "|".PadRight(5), outcomes(1).PadRight(20), "|")
+        Console.WriteLine("".PadRight(90, "_"))
+        Console.WriteLine("{0}{1}{2}{3}{4}{5}{6}{7}", "|".PadRight(5), "3".PadRight(20), "|".PadRight(5), numtoword(usermoves(0)).ToString.PadRight(20), "|".PadRight(5), numtoword(computermoves(2)).ToString.PadRight(20), "|".PadRight(5), outcomes(2).PadRight(20), "|")
+        Console.WriteLine("".PadRight(90, "_"))
+        Console.WriteLine("{0}{1}{2}{3}{4}{5}{6}{7}", "|".PadRight(5), "4".PadRight(20), "|".PadRight(5), numtoword(usermoves(0)).ToString.PadRight(20), "|".PadRight(5), numtoword(computermoves(3)).ToString.PadRight(20), "|".PadRight(5), outcomes(3).PadRight(20), "|")
+        Console.WriteLine("".PadRight(90, "_"))
+        Console.WriteLine("{0}{1}{2}{3}{4}{5}{6}{7}", "|".PadRight(5), "5".PadRight(20), "|".PadRight(5), numtoword(usermoves(0)).ToString.PadRight(20), "|".PadRight(5), numtoword(computermoves(4)).ToString.PadRight(20), "|".PadRight(5), outcomes(4).PadRight(20), "|")
+        Console.WriteLine("".PadRight(90, "_"))
     End Sub
 
 End Module
